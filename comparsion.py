7459925b84d48d69a4f38bc32c599331814b715f
Kirti.S <97077407+kirtis12@users.noreply.github.com>
2022-01-30 21:37:35 +0530

    Update comparsion.py

--- comparsion.py
+++ comparsion.py
@@ -1,19 +1,20 @@
 a=int(input("enter a number for a:"))
 b=int(input("enter a number for b:"))
-c=a+b
-print("addition",c)
-
-a=int(input("enter a number for a:"))
-b=int(input("enter a number for b:"))
-c=a-b
-print("sub",c)
-
-a=int(input("enter a number for a:"))
-b=int(input("enter a number for b:"))
-c=a*b
-print("multiplication",c)
-
-a=int(input("enter a number for a:"))
-b=int(input("enter a number for b:"))
-c=a//b
-print("floor division",c)
+choice= int(input("enter a number between 1-5 .1.add, 2.sub, 3.muliplication, 4.division, 5. floor divion"))
+if choice==1:
+    c=a+b
+    print("addition",c)
+elif choice==2:
+    c=a-b
+    print("sub",c)
+elif choice==3:
+    c=a*b
+    print("multiplication",c)
+elif choice==4:
+    c=a/b
+    print("division",c)
+elif choice==5:
+    c=a//b
+    print("floor division",c)
+else:
+    print("invalid option")
